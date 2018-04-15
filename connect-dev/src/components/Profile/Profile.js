d855789224f4fe95bf7d0675e886cea73b9a5514
Aaron Blackshear <theshiftybapple@gmail.com>
2018-04-15 14:41:49 -0500

    Safty commit

--- connect-dev/src/components/Profile/Profile.js
+++ connect-dev/src/components/Profile/Profile.js
@@ -5,27 +5,28 @@ import { connect } from "react-redux";
 
 class Profile extends Component {
   render() {
-    const { name, email, profilePic } = this.props;
+    const { name, email, profilePic,bio } = this.props;
 
     return (
       <div>
-        <div className="profile-banner" />
-        <img src={profilePic} className="profile-pic" />
-        <section className="user-info">
-          <h2 className="user-name">{name || "Placeholder"}</h2>
-          <p className='info-title'>User Type: </p>
-          <p className="user-type">Developer</p>
-          <p className='info-title'>Email: </p>
-          <p className="user-email">{email || "Email"}</p>
-          <p className='info-title'>Bio: </p>
-          <p className="user-bio">Enter Bio</p>
-          <p className='info-title'>Experience: </p>
-          <p className="user-experience">Enter Experience</p>
-        </section>
-        <section className="posts-container">
-          <h2 className="posts-title">Previous Posts</h2>
-          <div className="post">Post</div>
-        </section>
+        <div className="profile-banner">
+          <img src={profilePic} className="profile-pic" />
+          <section className="user-info">
+            <h2 className="user-name">{name || "Placeholder"}</h2>
+            <p className="info-title">User Type: </p>
+            <p className="info">Developer</p>
+            <p className="info-title">Email: </p>
+            <p className="info">{email || "User has no email"}</p>
+            <p className="info-title">Bio: </p>
+            { !bio ? <input placeholder='Enter Bio' /> :<p className="info">{bio}</p> }
+            <p className="info-title">Experience: </p>
+            <p className="info">User has no experience</p>
+          </section>
+          <section className="posts-container">
+            <h2 className="posts-title">Previous Posts</h2>
+            <div className="post">Post</div>
+          </section>
+        </div>
       </div>
     );
   }
