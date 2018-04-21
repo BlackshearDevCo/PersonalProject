77fd4aa06543fb0709250b33b2824404657c7b50
Aaron Blackshear <theshiftybapple@gmail.com>
2018-04-21 09:00:30 -0500

    Fixed Navbar animations

--- connect-dev/src/components/Navbar/Navbar.js
+++ connect-dev/src/components/Navbar/Navbar.js
@@ -13,41 +13,41 @@ class Navbar extends Component {
 
     return (
       <div className='nav'>
-        <div className={!menuFlag ? 'nav-pfp' : 'nav-pfp-hide'} />
+        <Link to='/profile' onClick={() => toggleMenuFlag()}><div className={!menuFlag ? 'nav-pfp' : 'nav-pfp-hide'} /></Link>
           <div className={!menuFlag ? "nav-links" : "nav-true"}>
-            <Link to="/" className="link" onClick={() => toggleMenuFlag()}>
+            <Link to="/" className={!menuFlag ? 'link-1' : 'link-1-active' } onClick={() => toggleMenuFlag()}>
               Home
             </Link>
-            <Link to="/devs" className="link" onClick={() => toggleMenuFlag()}>
+            <Link to="/devs" className={!menuFlag ? 'link-2' : 'link-2-active' } onClick={() => toggleMenuFlag()}>
               Devs
             </Link>
             <Link
               to="/employers"
-              className="link"
+              className={!menuFlag ? 'link-3' : 'link-3-active' }
               onClick={() => toggleMenuFlag()}
             >
               Employers
             </Link>
             <Link
               to="/community"
-              className="link"
+              className={!menuFlag ? 'link-4' : 'link-4-active' }
               onClick={() => toggleMenuFlag()}
             >
               Community
             </Link>
             <Link
               to="/profile"
-              className="link"
+              className={!menuFlag ? 'link-5' : 'link-5-active' }
               onClick={() => toggleMenuFlag()}
             >
               Profile
             </Link>
-            <Link to="/about" className="link" onClick={() => toggleMenuFlag()}>
+            <Link to="/about" className={!menuFlag ? 'link-6' : 'link-6-active' } onClick={() => toggleMenuFlag()}>
               About
             </Link>
             <Link
               to="/contact"
-              className="link"
+              className={!menuFlag ? 'link-7' : 'link-7-active' }
               onClick={() => toggleMenuFlag()}
             >
               Contact
@@ -55,7 +55,7 @@ class Navbar extends Component {
             {!this.props.name ? (
               <a
                 href="http://localhost:3001/auth"
-                className="link"
+                className={!menuFlag ? 'link-8' : 'link-8-active' }
                 onClick={() => toggleMenuFlag()}
               >
                 Login
@@ -63,7 +63,7 @@ class Navbar extends Component {
             ) : (
               <a href='http://localhost:3000/#/'
                 onClick={() => {this.props.logout(), toggleMenuFlag()}}
-                className="link"
+                className={!menuFlag ? 'link-8' : 'link-8-active' }
               >
                 Logout
               </a>
