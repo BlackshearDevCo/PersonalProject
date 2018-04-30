b1a3b700eb348e1eda10e8013121885fc8ce4ffa
Aaron Blackshear <theshiftybapple@gmail.com>
2018-04-30 10:02:21 -0500

    Nodemailer is working, not finished

--- connect-dev/server/index.js
+++ connect-dev/server/index.js
@@ -65,7 +65,12 @@ passport.serializeUser((user, done) => {
       if (!response[0]) {
         app
           .get("db")
-          .addUserByAuthId([user.displayName, user.id, user.picture, user.emails[0].value])
+          .addUserByAuthId([
+            user.displayName,
+            user.id,
+            user.picture,
+            user.emails[0].value
+          ])
           .then(res => {
             return done(null, res[0]);
           })
@@ -85,7 +90,7 @@ app.get(
   "/auth",
   passport.authenticate("auth0", {
     successRedirect: "http://localhost:3000/#/",
-    failureRedirect: "http://localhost:3001/auth",
+    failureRedirect: "http://localhost:3001/auth"
   })
 );
 
@@ -97,32 +102,74 @@ function authenticate(req, res, next) {
   }
 }
 
-app.get('/api/logout', userCtrl.logout);
-app.get('/api/user', userCtrl.getUser);
-app.get('/api/getPosts', postCtrl.getPosts);
-app.get('/api/getEmployersPosts', postCtrl.getEmployersPosts);
-app.get('/api/getAllUsers/:id', userCtrl.getAllUsers);
-app.get('/api/getUserPosts/:id', postCtrl.getUserPosts);
-app.get('/api/getConnectionCount/:id', userCtrl.getUserConnections);
-app.get('/api/getNotifications/:id', userCtrl.getNotifications);
-app.post('/api/connectWithUser/:id', userCtrl.connectWithUser);
-app.post('/api/newPost', postCtrl.newPost);
-app.put('/api/updateUserInfo/:id', userCtrl.updateUserInfo);
-app.put('/api/sendNotification/:id', userCtrl.sendNotification);
-app.delete('/api/deletePost/:id', postCtrl.deletePost);
+app.get("/api/logout", userCtrl.logout);
+app.get("/api/user", userCtrl.getUser);
+app.get("/api/getPosts", postCtrl.getPosts);
+app.get("/api/getEmployersPosts", postCtrl.getEmployersPosts);
+app.get("/api/getAllUsers/:id", userCtrl.getAllUsers);
+app.get("/api/getUserPosts/:id", postCtrl.getUserPosts);
+app.get("/api/getConnectionCount/:id", userCtrl.getUserConnections);
+app.get("/api/getNotifications/:id", userCtrl.getNotifications);
+app.post("/api/connectWithUser/:id", userCtrl.connectWithUser);
+app.post("/api/newPost", postCtrl.newPost);
+app.put("/api/updateUserInfo/:id", userCtrl.updateUserInfo);
+app.put("/api/sendNotification/:id", userCtrl.sendNotification);
+app.delete("/api/deletePost/:id", postCtrl.deletePost);
 
 const port = 3001;
 let server = app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 //WEB SOCKETS
 
-const socket = require('socket.io');
+const socket = require("socket.io");
 io = socket(server);
 
-io.on('connection', socket => {
+io.on("connection", socket => {
   console.log(socket.id);
 
-  socket.on('SEND_MESSAGE', data => {
-    io.emit('RECIEVE_MESSAGE', data)
-  })
-})
+  socket.on("SEND_MESSAGE", data => {
+    io.emit("RECIEVE_MESSAGE", data);
+  });
+});
+
+//NODEMAILER
+
+const nodemailer = require("nodemailer");
+
+app.post("/api/sendMail", () => {
+  nodemailer.createTestAccount((err, account) => {
+    console.log(account);
+    let transporter = nodemailer.createTransport({
+      service: "Gmail",
+      secure: false,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS
+      }
+    });
+
+    let mailOptions = {
+      to: "theshiftybapple@gmail.com",
+      subject: "ConnectDev",
+      test: `Aaron Blackshear wants to Connect!`,
+      html: `<p>Hello Jasmine! Aaron Blackshear wants to get in touch with you. Check out their profile <a href='http://localhost:3000/#/user/8'>here</a>!
+      If you like them, don't hesitate to get back in touch with them. Their email is: theshiftybapple@gmail.com <br>
+      Have a great day!
+      </p>`
+    };
+    // let mailOptions = {
+    //   to: reciever,
+    //   subject: 'ConnectDev',
+    //   test: `${user} wants to Connect!`,
+    //   html: `<p>Hello ${recieverName}! ${sender} wants to get in touch with you. Check out their profile <a href='${link}'>here</a>!</p>`
+    // };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(info.messageId);
+      console.log(nodemailer.getTestMessageUrl(info));
+    });
+  });
+});
